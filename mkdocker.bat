b4ed9ba105d9c779d3b73681f69d23510a308399
Chris Panetta <cv.panetta@gmail.com>
2024-11-05 20:11:04 -0500

    changed port again

--- mkdocker.bat
+++ mkdocker.bat
@@ -4,5 +4,5 @@ docker stop dian-wsp2
 docker rm dian-wsp2
 cd C:\Users\Administrator\Documents\Dian\Dian-WSP2
 docker build -t dian-wsp2 .
-docker run -d -p 3334:3000 --name dian-wsp2 dian-wsp2
+docker run -d -p 3334:4000 --name dian-wsp2 dian-wsp2
 pause
