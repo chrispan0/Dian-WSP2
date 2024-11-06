16f6409af60e777604ce470955cf13f93a103c37
Chris Panetta <cv.panetta@gmail.com>
2024-11-05 20:04:18 -0500

    made it dian specific

--- mkdocker.bat
+++ mkdocker.bat
@@ -1,8 +1,8 @@
 git fetch
 git pull
-docker stop barney-wsp2
-docker rm barney-wsp2
-cd C:\Users\Administrator\Documents\Barney\Barney-WSP2
-docker build -t barney-wsp2 .
-docker run -d -p 3333:3000 --name barney-wsp2 barney-wsp2
+docker stop dian-wsp2
+docker rm dian-wsp2
+cd C:\Users\Administrator\Documents\Dian\Dian-WSP2
+docker build -t dian-wsp2 .
+docker run -d -p 3334:3000 --name dian-wsp2 dian-wsp2
 pause
