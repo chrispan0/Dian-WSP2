9688f6daf6648a402e9312f56003c4e64ff6c8a6
Dian4285 <ardian.kent@ontariotechu.net>
2024-11-05 19:44:38 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const indexRouter = require('./routes/index');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+module.exports = app;
